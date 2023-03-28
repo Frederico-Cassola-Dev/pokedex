da1f8789fc2cdba054f4af64b320745b0733626d
Frederico Cassola <cfcassola@gmail.com>
2023-03-28 11:12:16 +0200

    Add condition to pokemonCard component

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,8 +1,23 @@
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
 const Figure = () => {
-    return <figure>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Image of pokemon" />
+  const pokemon = pokemonList[1];
+
+    return (
+      <figure>
+        { pokemon.imgSrc !== undefined  ? <img src={pokemon.imgSrc} alt="Image of pokemon" /> : <p>???</p>}
         <figcaption>bulbasaur</figcaption>
-    </figure>
-}
+      </figure>
+    );
+};
 
 export default Figure;
